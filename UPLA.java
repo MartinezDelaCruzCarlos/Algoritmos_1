5638f4bb8b5b0318e0c7a4ecd7a67543dff0d5ba
MartinezDelaCruzCarlos <113785619+MartinezDelaCruzCarlos@users.noreply.github.com>
2022-09-20 18:37:14 -0500

    Add files via upload

---
+++ UPLA.java
@@ -0,0 +1,12 @@
+
+package upla;
+/**
+ * @author Martínez De la Cruz Carlos
+ */
+public class UPLA {
+//1) Realizar un programa que permita mostrar en pantalla el mensaje “UNIVERSIDAD PERUANA LOS ANDES”
+    public static void main(String[] args) {
+        System.out.println("UNIVERSIDA PERUANA LOS ANDES");
+    }
+    
+}
