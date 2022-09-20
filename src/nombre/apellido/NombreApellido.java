db7de4682931fac165931a924c30d01f6613bda6
USER <USER@AVATEC>
2022-09-20 17:30:25 -0500

    Tarea num:2

---
+++ src/nombre/apellido/NombreApellido.java
@@ -0,0 +1,13 @@
+
+package nombre.apellido;
+/**
+ * @author Martínez De la Cruz Carlos
+ */
+public class NombreApellido {
+
+   
+    public static void main(String[] args) {
+        System.out.println("MARTINEZ DE LA CRUZ CARLOS FERNANDO");
+    }
+    
+}
